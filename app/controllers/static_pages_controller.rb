322559c9b0549d50f6459e7ad4515208fdf1bad6
Raymond Oh <raymond.oh@gmail.com>
2020-10-17 14:22:36 -0400

    add CSS for responsive thumbnail grid layout; images link to larger _blank image

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,17 @@ require 'flickr'
 
 class StaticPagesController < ApplicationController
   def index
+    flickr = Flickr.new ENV['FLICKR_API_KEY'], ENV['FLICKR_SECRET']
 
+    if params[:user_id]
+      @photos = flickr.photos.search(user_id: params[:user_id])
+      @urls = @photos.map do |photo|
+        {
+          thumb: "https://live.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}_q.jpg",
+          link: "https://live.staticflickr.com/#{photo['server']}/#{photo['id']}_#{photo['secret']}.jpg"
+        }
+      end
+    end
+  
   end
 end
